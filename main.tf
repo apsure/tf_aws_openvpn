e802567de0bb4f3ac248b32851132cbd5ca272e5
Nathan Palmer <nathanpalmer@users.noreply.github.com>
2019-07-10 12:54:06 -0400

    Try to use the first subnet id

--- main.tf
+++ main.tf
@@ -47,11 +47,10 @@ resource "aws_security_group" "openvpn" {
 }
 
 resource "aws_instance" "openvpn" {
-  count         = "1"
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
   key_name      = "${var.key_name}"
-  subnet_id     = "${element(var.public_subnet_ids, count.index)}"
+  subnet_id     = "${element(var.public_subnet_ids, 0)}"
 
   vpc_security_group_ids = ["${aws_security_group.openvpn.id}"]
 
