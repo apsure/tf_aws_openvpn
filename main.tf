d622d31c2fac560e2f46e76a9ee3636a21d26672
Nathan Palmer <nathanpalmer@users.noreply.github.com>
2019-07-10 12:42:20 -0400

    setting the count to 1 for terraform 0.12 compatibility

--- main.tf
+++ main.tf
@@ -47,6 +47,7 @@ resource "aws_security_group" "openvpn" {
 }
 
 resource "aws_instance" "openvpn" {
+  count         = "1"
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
   key_name      = "${var.key_name}"
